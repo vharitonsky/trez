1c6b6c28cef89dab203b5229a7176427442f06c9
12345656 <12345656@123>
2016-10-01 16:43:44 +0300

    try free unsafe pointer

--- opencv.go
+++ opencv.go
@@ -194,7 +194,6 @@ func Resize(data []byte, options Options) ([]byte, error) {
 	ptr := C.ptr_from_mat(ret)
 	data = C.GoBytes(unsafe.Pointer(ptr), ret.step)
 	C.cvReleaseMat(&ret)
-	C.free(unsafe.Pointer(dst))
 
 	return data, nil
 }
